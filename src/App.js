e55c60f52fd6618a50a0b73720c4a281f2276376
Siva <byteshiva@users.noreply.github.com>
2022-05-20 21:07:45 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import JsonDataDisplay from './MyPractice/GeekTable'
+function App() {
+return (
+	<div className="App">
+	<h1>Mass Shooting USA!!!</h1>
+	<JsonDataDisplay/>
+	</div>
+);
+}
+
+export default App;
