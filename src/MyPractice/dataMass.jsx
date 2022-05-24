a776c6da747dcfd15a1788f723158a735859939a
Siva <byteshiva@users.noreply.github.com>
2022-05-24 20:11:04 +0530

    Convert USA State Acronym to State Name
    - Convert USA State Acronym to State Name
    - get State Name from states_hash.json, pass the acronym to JSON hash

--- src/MyPractice/dataMass.jsx
+++ src/MyPractice/dataMass.jsx
@@ -1,17 +1,20 @@
 import axios from "axios";
 import {useState, useEffect}   from "react";
+import StateUSA_ACR from './states_hash.json'
 import moment from "moment";
 
 function Mass() {
     const [mass, setMass] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
+    const [stateusa, setStateusa] = useState([]);
     
 
     const handleMass = (mass) => {
         axios.get("https://mass-shooting-tracker-data.s3.us-east-2.amazonaws.com/2022-data.json")
         .then(response => {
             setMass(response.data);
+            setStateusa(StateUSA_ACR);
             setLoading(false);
         }).catch(err => {
             console.log(err);
@@ -35,7 +38,7 @@ function Mass() {
         return (
             <tr key={index+1}>
                 <td>{datenow}</td>
-                <td>{data.state}</td>
+                <td>{stateusa[data.state]}</td>
                 <td>{data.city}</td>
                 <td>{data.killed}</td>
                 <td>{sources}</td>    
@@ -46,8 +49,8 @@ function Mass() {
     return ( 
         <div className="container">
         {loading && <div>Loading data please wait..</div>}
-        <div > 
-            <table table table-striped>
+        <div> 
+            <table table table-striped >
                 <thead>
                     <tr>
                         <th>Date</th>
