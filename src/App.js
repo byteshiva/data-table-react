3ef1675c16da6194eef9f56699a6ac05e78812e4
Siva <byteshiva@gmail.com>
2022-06-08 12:49:18 +0530

    clean up, remove unused imports, remove unused files

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
 import './App.css';
 
-// import JsonDataDisplay from './MyPractice/GeekTable'
-// import MassData from './MyPractice/MassData'
-// import Mass from './MyPractice/dataMass';
-// import { CoinFlip5} from './MyPractice/CoinFlip';
-
-// import Repeater from './components/Links'
-// import HeaderLinks from './components/HeaderLinks';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-// import Home from './pages/Home';
 import Products from './pages/Products';
 import Reports from './pages/Reports';
 import Teams from './pages/Teams';
 import Support from './pages/Support';
 import Messages from './pages/Messages';
-// import Main from './components/Main';
-// import { useState } from 'react';
 import NoMatch from './pages/NoMatch';
 import { IdGuardWrapper } from './pages/IdGuardWrapper';
 
-// import Footer from './components/FooterFirst';
-// import Button from './components/Button'
-// const Header = () => <header>header</header>;
-// const Footer = () => <footer>footer</footer>;
-
-
 function App() {
-// const [isUserAuthenticated,SetIsUserAuthenticated] = useState(false);
-// SetIsUserAuthenticated(!isUserAuthenticated);
 return (
 	<>
 		<Router>
