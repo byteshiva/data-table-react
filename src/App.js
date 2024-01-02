a6541c727205fcff5e1b13c9939645655d485875
Siva <byteshiva@users.noreply.github.com>
2024-01-02 09:15:39 +0530

    chore: Dynamically set default route based on current year
    - Modify App component to use the current year obtained from the client's browser
    - Redirect to the '/table/:year' route with the current year as the default
    - Improve code readability and maintainability

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import './App.css';
-
-import Navbar from './components/Navbar';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Products from './pages/Products';
 import Reports from './pages/Reports';
 import Teams from './pages/Teams';
@@ -10,48 +9,38 @@ import Messages from './pages/Messages';
 import NoMatch from './pages/NoMatch';
 import Dashboard from './pages/Dashboard';
 import { IdGuardWrapper } from './pages/IdGuardWrapper';
-// import Footer from './components/Footer';
 
 function App() {
-return (
-	<>
-		<Router>
-			<Navbar />
-			<Routes>
-				{/* <Route
-					exact
-					path="/"
-					render={() => {
-						return (
-							isUserAuthenticated ?
-							<Navigate to="/support"></Navigate> :
-							<Navigate to="/table/2022"> </Navigate>
-						)
-					}}
-				/> */}
-				<Route exact path="/" element={<Navigate to="/table/2023" replace={true}> </Navigate>} />
+  const currentYear = new Date().getFullYear();
 
-				  <Route exact
-						path='table/:id'
-						element={(
-						<IdGuardWrapper> 
-						</IdGuardWrapper>
-						)}
-  				/>
-				<Route path="/dashboard" element={<Dashboard />} />
-				<Route path="/products" element={<Products />} />
-				<Route path="/reports" element={<Reports />} />
-				{/* <Route path="/table/:id" element={ <Main />} /> */}
-				<Route path="/teams" element={<Teams/>} />
-				<Route path="/support" element={<Support />} />
-				<Route path="/Messages" element={<Messages />} />
-				<Route path="*" element={<NoMatch />} />
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={<Navigate to={`/table/${currentYear}`} replace={true}></Navigate>}
+          />
 
-			</Routes>
-		</Router>
-		{/* <Footer /> */}
-	</>
-);
+          <Route
+            exact
+            path="table/:id"
+            element={<IdGuardWrapper></IdGuardWrapper>}
+          />
+
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/reports" element={<Reports />} />
+          <Route path="/teams" element={<Teams />} />
+          <Route path="/support" element={<Support />} />
+          <Route path="/Messages" element={<Messages />} />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
